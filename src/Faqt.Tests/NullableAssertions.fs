5ebeef106ee9761e95ca9dbb8c10dd77f9e6a908
Christer van der Meeren <cmeeren@protonmail.com>
2023-07-29 23:10:33 +0200

    Add Nullable assertions

---
+++ src/Faqt.Tests/NullableAssertions.fs
@@ -0,0 +1,155 @@
+﻿module NullableAssertions
+
+open System
+open Faqt
+open Xunit
+
+
+module HaveValue =
+
+
+    [<Fact>]
+    let ``Passes for nullable values that have a value and can be chained with AndDerived with inner value`` () =
+        Nullable(1)
+            .Should()
+            .HaveValue()
+            .Id<AndDerived<Nullable<int>, int>>()
+            .That.Should()
+            .Be(1)
+
+
+    [<Fact>]
+    let ``Fails with expected message for nullable values without a value`` () =
+        fun () ->
+            let x = Nullable<int>()
+            x.Should().HaveValue()
+        |> assertExnMsg
+            """
+x
+    should have a value, but was
+<null>
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = Nullable<int>()
+            x.Should().HaveValue("some reason")
+        |> assertExnMsg
+            """
+x
+    should have a value because some reason, but was
+<null>
+"""
+
+
+module NotHaveValue =
+
+
+    [<Fact>]
+    let ``Passes for nullable values that do not have a value and can be chained with And`` () =
+        Nullable<int>()
+            .Should()
+            .NotHaveValue()
+            .Id<And<Nullable<int>>>()
+            .And.Be(Nullable())
+
+
+    [<Fact>]
+    let ``Fails with expected message for nullable values with a value`` () =
+        fun () ->
+            let x = Nullable(1)
+            x.Should().NotHaveValue()
+        |> assertExnMsg
+            """
+x
+    should not have a value, but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = Nullable(1)
+            x.Should().NotHaveValue("some reason")
+        |> assertExnMsg
+            """
+x
+    should not have a value because some reason, but was
+1
+"""
+
+
+module BeNull =
+
+
+    [<Fact>]
+    let ``Passes for nullable values that do not have a value and can be chained with And`` () =
+        Nullable<int>().Should().BeNull().Id<And<Nullable<int>>>().And.Be(Nullable())
+
+
+    [<Fact>]
+    let ``Fails with expected message for nullable values with a value`` () =
+        fun () ->
+            let x = Nullable(1)
+            x.Should().BeNull()
+        |> assertExnMsg
+            """
+x
+    should be null, but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = Nullable(1)
+            x.Should().BeNull("some reason")
+        |> assertExnMsg
+            """
+x
+    should be null because some reason, but was
+1
+"""
+
+
+module NotBeNull =
+
+
+    [<Fact>]
+    let ``Passes for nullable values that have a value and can be chained with AndDerived with inner value`` () =
+        Nullable(1)
+            .Should()
+            .NotBeNull()
+            .Id<AndDerived<Nullable<int>, int>>()
+            .That.Should()
+            .Be(1)
+
+
+    [<Fact>]
+    let ``Fails with expected message for nullable values without a value`` () =
+        fun () ->
+            let x = Nullable<int>()
+            x.Should().NotBeNull()
+        |> assertExnMsg
+            """
+x
+    should not be null, but was
+<null>
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = Nullable<int>()
+            x.Should().NotBeNull("some reason")
+        |> assertExnMsg
+            """
+x
+    should not be null because some reason, but was
+<null>
+"""
---
+++ src/Faqt/NullableAssertions.fs
@@ -0,0 +1,55 @@
+﻿namespace Faqt
+
+open System
+open System.Runtime.CompilerServices
+open AssertionHelpers
+
+
+[<Extension>]
+type NullableAssertions =
+
+
+    /// Asserts that the subject has a value.
+    [<Extension>]
+    static member HaveValue(t: Testable<Nullable<'a>>, ?because) : AndDerived<Nullable<'a>, 'a> =
+        use _ = t.Assert()
+
+        if not t.Subject.HasValue then
+            t.Fail("{subject}\n\tshould have a value{because}, but was\n{actual}", because)
+
+        AndDerived(t, t.Subject.Value)
+
+
+    /// Asserts that the subject has a value.
+    [<Extension>]
+    static member NotHaveValue(t: Testable<Nullable<'a>>, ?because) : And<Nullable<'a>> =
+        use _ = t.Assert()
+
+        if t.Subject.HasValue then
+            t.Fail("{subject}\n\tshould not have a value{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is null (i.e., does not have a value). Equivalent to NotHaveValue (but with a different
+    /// error message).
+    [<Extension>]
+    static member BeNull(t: Testable<Nullable<'a>>, ?because) : And<Nullable<'a>> =
+        use _ = t.Assert()
+
+        if t.Subject.HasValue then
+            t.Fail("{subject}\n\tshould be null{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is not null (i.e., has a value). Equivalent to NotHaveValue (but with a different error
+    /// message).
+    [<Extension>]
+    static member NotBeNull(t: Testable<Nullable<'a>>, ?because) : AndDerived<Nullable<'a>, 'a> =
+        use _ = t.Assert()
+
+        if not t.Subject.HasValue then
+            t.Fail("{subject}\n\tshould not be null{because}, but was\n{actual}", because)
+
+        AndDerived(t, t.Subject.Value)
