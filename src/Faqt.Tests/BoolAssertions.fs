4979d2f0337cd4d44fb70c4dab03658244102a92
Christer van der Meeren <cmeeren@protonmail.com>
2023-07-29 22:19:43 +0200

    Add boolean assertions

---
+++ src/Faqt.Tests/BoolAssertions.fs
@@ -0,0 +1,172 @@
+﻿module BoolAssertions
+
+open Faqt
+open Xunit
+
+
+module BeTrue =
+
+
+    [<Fact>]
+    let ``Passes for true values can be chained with And`` () =
+        true.Should().BeTrue().Id<And<bool>>().And.Be(true)
+
+
+    [<Fact>]
+    let ``Fails with expected message for false values`` () =
+        fun () ->
+            let x = false
+            x.Should().BeTrue()
+        |> assertExnMsg
+            """
+x
+    should be
+true
+    but was
+false
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = false
+            x.Should().BeTrue("some reason")
+        |> assertExnMsg
+            """
+x
+    should be
+true
+    because some reason, but was
+false
+"""
+
+
+module BeFalse =
+
+
+    [<Fact>]
+    let ``Passes for false values can be chained with And`` () =
+        false.Should().BeFalse().Id<And<bool>>().And.Be(false)
+
+
+    [<Fact>]
+    let ``Fails with expected message for true values`` () =
+        fun () ->
+            let x = true
+            x.Should().BeFalse()
+        |> assertExnMsg
+            """
+x
+    should be
+false
+    but was
+true
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = true
+            x.Should().BeFalse("some reason")
+        |> assertExnMsg
+            """
+x
+    should be
+false
+    because some reason, but was
+true
+"""
+
+
+module Imply =
+
+
+    [<Fact>]
+    let ``Passes if subject is false and other is false and can be chained with And`` () =
+        false.Should().Imply(false).Id<And<bool>>().And.Be(false)
+
+
+    [<Fact>]
+    let ``Passes if subject is false and other is true`` () = false.Should().Imply(true)
+
+
+    [<Fact>]
+    let ``Passes if subject is true and other is true`` () = true.Should().Imply(true)
+
+
+    [<Fact>]
+    let ``Fails with expected message if subject is true and other is false`` () =
+        fun () ->
+            let x = true
+            x.Should().Imply(false)
+        |> assertExnMsg
+            """
+x
+    should imply the specified value, but subject was
+true
+    and the specified value was
+false
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = true
+            x.Should().Imply(false, "some reason")
+        |> assertExnMsg
+            """
+x
+    should imply the specified value because some reason, but subject was
+true
+    and the specified value was
+false
+"""
+
+
+module BeImpliedBy =
+
+
+    [<Fact>]
+    let ``Passes if subject is false and other is false and can be chained with And`` () =
+        false.Should().BeImpliedBy(false).Id<And<bool>>().And.Be(false)
+
+
+    [<Fact>]
+    let ``Passes if subject is true and other is false`` () = true.Should().BeImpliedBy(false)
+
+
+    [<Fact>]
+    let ``Passes if subject is true and other is true`` () = true.Should().BeImpliedBy(true)
+
+
+    [<Fact>]
+    let ``Fails with expected message if subject is false and other is true`` () =
+        fun () ->
+            let x = false
+            x.Should().BeImpliedBy(true)
+        |> assertExnMsg
+            """
+x
+    should be implied by the specified value, but the value was
+true
+    and the subject was
+false
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = false
+            x.Should().BeImpliedBy(true, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be implied by the specified value because some reason, but the value was
+true
+    and the subject was
+false
+"""
---
+++ src/Faqt/BoolAssertions.fs
@@ -0,0 +1,53 @@
+﻿namespace Faqt
+
+open System.Runtime.CompilerServices
+open AssertionHelpers
+open Formatting
+
+
+[<Extension>]
+type BoolAssertions =
+
+
+    /// Asserts that the subject is true. Alias of Be(true).
+    [<Extension>]
+    static member BeTrue(t: Testable<bool>, ?because) : And<bool> =
+        use _ = t.Assert()
+        t.Be(true, ?because = because)
+
+
+    /// Asserts that the subject is true. Alias of Be(false).
+    [<Extension>]
+    static member BeFalse(t: Testable<bool>, ?because) : And<bool> =
+        use _ = t.Assert()
+        t.Be(false, ?because = because)
+
+
+    /// Asserts that if the subject is true, other is also true.
+    [<Extension>]
+    static member Imply(t: Testable<bool>, other: bool, ?because) : And<bool> =
+        use _ = t.Assert()
+
+        if t.Subject && not other then
+            t.Fail(
+                "{subject}\n\tshould imply the specified value{because}, but subject was\n{actual}\n\tand the specified value was\n{0}",
+                because,
+                format other
+            )
+
+        And(t)
+
+
+    /// Asserts that if other is true, subject is also true.
+    [<Extension>]
+    static member BeImpliedBy(t: Testable<bool>, other: bool, ?because) : And<bool> =
+        use _ = t.Assert()
+
+        if other && not t.Subject then
+            t.Fail(
+                "{subject}\n\tshould be implied by the specified value{because}, but the value was\n{0}\n\tand the subject was\n{actual}",
+                because,
+                format other
+            )
+
+        And(t)
