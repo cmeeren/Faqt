4030d5a9bde376654e972cca5f61cce54d64a64b
Christer van der Meeren <cmeeren@protonmail.com>
2023-07-30 19:07:11 +0200

    Add greater/lower/negative/positive/range assertions

--- src/Faqt.Tests/ComparisonAssertions.fs
+++ src/Faqt.Tests/ComparisonAssertions.fs
@@ -179,3 +179,519 @@ x
     because some reason, but was
 1.02
 """
+
+
+module BeGreaterThan =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (1).Should().BeGreaterThan(0).Id<And<int>>().And.Be(1)
+
+
+    [<Fact>]
+    let ``Passes if subject > other`` () = (1).Should().BeGreaterThan(0)
+
+
+    [<Fact>]
+    let ``Fails if subject = other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (0).Should().BeGreaterThan(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if subject < other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (-1).Should().BeGreaterThan(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message for ints`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeGreaterThan(0)
+        |> assertExnMsg
+            """
+x
+    should be greater than
+0
+    but was
+-1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message for TimeSpan`` () =
+        fun () ->
+            let x = TimeSpan(0, 4, 0)
+            x.Should().BeGreaterThan(TimeSpan(0, 5, 0))
+        |> assertExnMsg
+            """
+x
+    should be greater than
+00:05:00
+    but was
+00:04:00
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeGreaterThan(0, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be greater than
+0
+    because some reason, but was
+-1
+"""
+
+
+module BeGreaterThanOrEqualTo =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (1).Should().BeGreaterThanOrEqualTo(0).Id<And<int>>().And.Be(1)
+
+
+    [<Fact>]
+    let ``Passes if subject > other`` () = (1).Should().BeGreaterThanOrEqualTo(0)
+
+
+    [<Fact>]
+    let ``Passes if subject = other`` () = (0).Should().BeGreaterThanOrEqualTo(0)
+
+
+    [<Fact>]
+    let ``Fails if subject < other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (-1).Should().BeGreaterThanOrEqualTo(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message for ints`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeGreaterThanOrEqualTo(0)
+        |> assertExnMsg
+            """
+x
+    should be greater than or equal to
+0
+    but was
+-1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message for TimeSpan`` () =
+        fun () ->
+            let x = TimeSpan(0, 4, 0)
+            x.Should().BeGreaterThanOrEqualTo(TimeSpan(0, 5, 0))
+        |> assertExnMsg
+            """
+x
+    should be greater than or equal to
+00:05:00
+    but was
+00:04:00
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeGreaterThanOrEqualTo(0, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be greater than or equal to
+0
+    because some reason, but was
+-1
+"""
+
+
+module BeLessThan =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (-1).Should().BeLessThan(0).Id<And<int>>().And.Be(-1)
+
+
+    [<Fact>]
+    let ``Passes if subject < other`` () = (-1).Should().BeLessThan(0)
+
+
+    [<Fact>]
+    let ``Fails if subject = other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (0).Should().BeLessThan(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if subject > other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (1).Should().BeLessThan(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message for ints`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeLessThan(0)
+        |> assertExnMsg
+            """
+x
+    should be less than
+0
+    but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message for TimeSpan`` () =
+        fun () ->
+            let x = TimeSpan(0, 5, 0)
+            x.Should().BeLessThan(TimeSpan(0, 4, 0))
+        |> assertExnMsg
+            """
+x
+    should be less than
+00:04:00
+    but was
+00:05:00
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeLessThan(0, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be less than
+0
+    because some reason, but was
+1
+"""
+
+
+module BeLessThanOrEqualTo =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (-1).Should().BeLessThanOrEqualTo(0).Id<And<int>>().And.Be(-1)
+
+
+    [<Fact>]
+    let ``Passes if subject < other`` () = (-1).Should().BeLessThanOrEqualTo(0)
+
+
+    [<Fact>]
+    let ``Passes if subject = other`` () = (0).Should().BeLessThanOrEqualTo(0)
+
+
+    [<Fact>]
+    let ``Fails if subject > other`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (1).Should().BeLessThanOrEqualTo(0) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message for ints`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeLessThanOrEqualTo(0)
+        |> assertExnMsg
+            """
+x
+    should be less than or equal to
+0
+    but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message for TimeSpan`` () =
+        fun () ->
+            let x = TimeSpan(0, 5, 0)
+            x.Should().BeLessThanOrEqualTo(TimeSpan(0, 4, 0))
+        |> assertExnMsg
+            """
+x
+    should be less than or equal to
+00:04:00
+    but was
+00:05:00
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeLessThanOrEqualTo(0, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be less than or equal to
+0
+    because some reason, but was
+1
+"""
+
+
+module BePositive =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (1).Should().BePositive().Id<And<int>>().And.Be(1)
+
+
+    [<Fact>]
+    let ``Passes if subject > 0`` () = (1).Should().BePositive()
+
+
+    [<Fact>]
+    let ``Fails if subject = 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (0).Should().BePositive() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if subject < 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (-1).Should().BePositive() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = -1
+            x.Should().BePositive()
+        |> assertExnMsg
+            """
+x
+    should be positive, but was
+-1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = -1
+            x.Should().BePositive("some reason")
+        |> assertExnMsg
+            """
+x
+    should be positive because some reason, but was
+-1
+"""
+
+
+module BeNegative =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (-1).Should().BeNegative().Id<And<int>>().And.Be(-1)
+
+
+    [<Fact>]
+    let ``Passes if subject < 0`` () = (-1).Should().BeNegative()
+
+
+    [<Fact>]
+    let ``Fails if subject = 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (0).Should().BeNegative() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if subject > 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (1).Should().BeNegative() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeNegative()
+        |> assertExnMsg
+            """
+x
+    should be negative, but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeNegative("some reason")
+        |> assertExnMsg
+            """
+x
+    should be negative because some reason, but was
+1
+"""
+
+
+module BeNonNegative =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (1).Should().BeNonNegative().Id<And<int>>().And.Be(1)
+
+
+    [<Fact>]
+    let ``Passes if subject > 0`` () = (1).Should().BeNonNegative()
+
+
+    [<Fact>]
+    let ``Passes if subject = 0`` () = (0).Should().BeNonNegative()
+
+
+    [<Fact>]
+    let ``Fails if subject < 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (-1).Should().BeNonNegative() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeNonNegative()
+        |> assertExnMsg
+            """
+x
+    should be non-negative, but was
+-1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = -1
+            x.Should().BeNonNegative("some reason")
+        |> assertExnMsg
+            """
+x
+    should be non-negative because some reason, but was
+-1
+"""
+
+
+module BeNonPositive =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (-1).Should().BeNonPositive().Id<And<int>>().And.Be(-1)
+
+
+    [<Fact>]
+    let ``Passes if subject < 0`` () = (-1).Should().BeNonPositive()
+
+
+    [<Fact>]
+    let ``Passes if subject = 0`` () = (0).Should().BeNonPositive()
+
+
+    [<Fact>]
+    let ``Fails if subject > 0`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (1).Should().BeNonPositive() |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeNonPositive()
+        |> assertExnMsg
+            """
+x
+    should be non-positive, but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeNonPositive("some reason")
+        |> assertExnMsg
+            """
+x
+    should be non-positive because some reason, but was
+1
+"""
+
+
+module BeInRange =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (2).Should().BeInRange(1, 3).Id<And<int>>().And.Be(2)
+
+
+    [<Fact>]
+    let ``Passes if subject is inside the range`` () = (2).Should().BeInRange(1, 3)
+
+
+    [<Fact>]
+    let ``Passes if subject is at the lower bound`` () = (1).Should().BeInRange(1, 3)
+
+
+    [<Fact>]
+    let ``Passes if subject is at the upper bound`` () = (3).Should().BeInRange(1, 3)
+
+
+    [<Fact>]
+    let ``Fails if subject is above the upper bound`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (4).Should().BeInRange(1, 3) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if subject is below the lower bound`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> (0).Should().BeInRange(1, 3) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeInRange(2, 4)
+        |> assertExnMsg
+            """
+x
+    should be in the range
+[2, 4]
+    but was
+1
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = 1
+            x.Should().BeInRange(2, 4, "some reason")
+        |> assertExnMsg
+            """
+x
+    should be in the range
+[2, 4]
+    because some reason, but was
+1
+"""
--- src/Faqt/ComparisonAssertions.fs
+++ src/Faqt/ComparisonAssertions.fs
@@ -45,3 +45,99 @@ type ComparisonAssertions =
             )
 
         And(t)
+
+
+    [<Extension>]
+    static member inline private Compare(t: Testable<'a>, op, opText, expected, because) =
+        if not (op t.Subject expected) then
+            t.Fail("{subject}\n\tshould be {0}\n{1}\n\t{because}but was\n{actual}", because, opText, format expected)
+
+        And(t)
+
+
+    /// Asserts that the subject is greater than the specified value.
+    [<Extension>]
+    static member inline BeGreaterThan(t: Testable<'a>, other: 'a, ?because) : And<'a> =
+        use _ = t.Assert()
+        t.Compare((>), "greater than", other, because)
+
+
+    /// Asserts that the subject is greater than or equal to the specified value.
+    [<Extension>]
+    static member inline BeGreaterThanOrEqualTo(t: Testable<'a>, other: 'a, ?because) : And<'a> =
+        use _ = t.Assert()
+        t.Compare((>=), "greater than or equal to", other, because)
+
+
+    /// Asserts that the subject is less than the specified value.
+    [<Extension>]
+    static member inline BeLessThan(t: Testable<'a>, other: 'a, ?because) : And<'a> =
+        use _ = t.Assert()
+        t.Compare((<), "less than", other, because)
+
+
+    /// Asserts that the subject is less than or equal to the specified value.
+    [<Extension>]
+    static member inline BeLessThanOrEqualTo(t: Testable<'a>, other: 'a, ?because) : And<'a> =
+        use _ = t.Assert()
+        t.Compare((<=), "less than or equal to", other, because)
+
+
+    /// Asserts that the subject is positive.
+    [<Extension>]
+    static member inline BePositive(t: Testable<'a>, ?because) : And<'a> =
+        use _ = t.Assert()
+
+        if t.Subject <= LanguagePrimitives.GenericZero then
+            t.Fail("{subject}\n\tshould be positive{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is negative.
+    [<Extension>]
+    static member inline BeNegative(t: Testable<'a>, ?because) : And<'a> =
+        use _ = t.Assert()
+
+        if t.Subject >= LanguagePrimitives.GenericZero then
+            t.Fail("{subject}\n\tshould be negative{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is non-negative (i.e., is zero or positive).
+    [<Extension>]
+    static member inline BeNonNegative(t: Testable<'a>, ?because) : And<'a> =
+        use _ = t.Assert()
+
+        if t.Subject < LanguagePrimitives.GenericZero then
+            t.Fail("{subject}\n\tshould be non-negative{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is non-positive (i.e., is zero or negative).
+    [<Extension>]
+    static member inline BeNonPositive(t: Testable<'a>, ?because) : And<'a> =
+        use _ = t.Assert()
+
+        if t.Subject > LanguagePrimitives.GenericZero then
+            t.Fail("{subject}\n\tshould be non-positive{because}, but was\n{actual}", because)
+
+        And(t)
+
+
+    /// Asserts that the subject is the specified inclusive range.
+    [<Extension>]
+    static member inline BeInRange(t: Testable<'a>, lower: 'a, upper: 'a, ?because) : And<'a> =
+        use _ = t.Assert()
+
+        if t.Subject < lower || t.Subject > upper then
+            t.Fail(
+                "{subject}\n\tshould be in the range\n[{0}, {1}]\n\t{because}but was\n{actual}",
+                because,
+                format lower,
+                format upper
+            )
+
+        And(t)
