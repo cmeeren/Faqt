1bba9362f35c65011372b00368c08c6649634981
Christer van der Meeren <cmeeren@protonmail.com>
2023-07-30 20:45:07 +0200

    Add string assertion HaveLength

---
+++ src/Faqt.Tests/StringAssertions.fs
@@ -0,0 +1,56 @@
+﻿module StringAssertions
+
+open Faqt
+open Xunit
+
+
+module HaveLength =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        "a".Should().HaveLength(1).Id<And<string>>().And.Be("a")
+
+
+    [<Fact>]
+    let ``Passes if length = expected`` () = "a".Should().HaveLength(1)
+
+
+    [<Fact>]
+    let ``Fails if length < expected`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> "".Should().HaveLength(1) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails if length > expected`` () =
+        Assert.Throws<AssertionFailedException>(fun () -> "as".Should().HaveLength(1) |> ignore)
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = ""
+            x.Should().HaveLength(1)
+        |> assertExnMsg
+            """
+x
+    should have length
+1
+    but was
+""
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = ""
+            x.Should().HaveLength(1, "some reason")
+        |> assertExnMsg
+            """
+x
+    should have length
+1
+    because some reason, but was
+""
+"""
---
+++ src/Faqt/StringAssertions.fs
@@ -0,0 +1,21 @@
+﻿namespace Faqt
+
+open System.Runtime.CompilerServices
+open AssertionHelpers
+open Formatting
+
+
+[<Extension>]
+type StringAssertions =
+
+
+    // TODO: Remove this after the corresponding IEnumerable method has been implemented?
+    /// Asserts that the subject has the specified length.
+    [<Extension>]
+    static member HaveLength(t: Testable<string>, expected: int, ?because) : And<string> =
+        use _ = t.Assert()
+
+        if t.Subject.Length <> expected then
+            t.Fail("{subject}\n\tshould have length\n{0}\n\t{because}but was\n{actual}", because, format expected)
+
+        And(t)
