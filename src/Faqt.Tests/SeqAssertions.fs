4d3a142e0979ea12ef57e0ec9135f6ad22091b74
Christer van der Meeren <cmeeren@protonmail.com>
2023-08-09 15:45:18 +0200

    Add SatisfyAll

---
+++ src/Faqt.Tests/SeqAssertions.fs
@@ -0,0 +1,66 @@
+﻿module SeqAssertions
+
+open Faqt
+open Xunit
+
+
+module SatisfyAll =
+
+
+    [<Fact>]
+    let ``Passes if all of the inner assertions passes and can be chained with And`` () =
+        [ "asd"; "123" ]
+            .Should()
+            .AllSatisfy(fun x -> x.Length.Should().Pass())
+            .Id<And<string list>>()
+            .And.Be([ "asd"; "123" ])
+
+
+    [<Fact>]
+    let ``Passes if subject is empty`` () =
+        List<int>.Empty.Should().AllSatisfy(fun x -> x.Should().Fail())
+
+
+    [<Fact>]
+    let ``Fails with expected message if at least one of the elements fail to satisfy the assertion`` () =
+        fun () ->
+            let x = [ "asd"; "test"; "1234" ]
+            x.Should().AllSatisfy(fun y -> y.Length.Should().Test(y.Length = 3))
+
+        |> assertExnMsg
+            """
+x
+    should only contain items satisfying the supplied assertion, but 2 of 3 items failed.
+
+[Item 2/3]
+
+y.Length
+
+[Item 3/3]
+
+y.Length
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = [ "asd"; "test"; "1234" ]
+
+            x
+                .Should()
+                .AllSatisfy((fun y -> y.Length.Should().Test(y.Length = 3)), "some reason")
+
+        |> assertExnMsg
+            """
+x
+    should only contain items satisfying the supplied assertion because some reason, but 2 of 3 items failed.
+
+[Item 2/3]
+
+y.Length
+
+[Item 3/3]
+
+y.Length
+"""
---
+++ src/Faqt/SeqAssertions.fs
@@ -0,0 +1,45 @@
+﻿namespace Faqt
+
+open System.Runtime.CompilerServices
+open AssertionHelpers
+
+
+[<Extension>]
+type SeqAssertions =
+
+
+    /// Asserts that all elements in the collection satisfy the supplied assertion.
+    [<Extension>]
+    static member AllSatisfy(t: Testable<#seq<'a>>, assertion: 'a -> 'ignored, ?because) : And<_> =
+        use _ = t.Assert(true, true)
+
+        let subjectLength = Seq.length t.Subject
+
+        let exceptions =
+            t.Subject
+            |> Seq.indexed
+            |> Seq.choose (fun (i, x) ->
+                try
+                    use _ = t.AssertItem()
+                    assertion x |> ignore
+                    None
+                with :? AssertionFailedException as ex ->
+                    Some(i, ex)
+            )
+            |> Seq.toArray
+
+        if exceptions.Length > 0 then
+            let assertionFailuresString =
+                exceptions
+                |> Seq.map (fun (i, ex) -> $"\n\n[Item %i{i + 1}/%i{subjectLength}]\n%s{ex.Message}")
+                |> String.concat ""
+
+            t.Fail(
+                "{subject}\n\tshould only contain items satisfying the supplied assertion{because}, but {0} of {1} items failed.{2}",
+                because,
+                string exceptions.Length,
+                string subjectLength,
+                assertionFailuresString
+            )
+
+        And(t)
