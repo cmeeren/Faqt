e89ed23f0370ff157d7637cb75c9b3797132570b
Christer van der Meeren <cmeeren@protonmail.com>
2023-08-10 11:18:29 +0200

    Remove unneeded string assertions (subsumed by corresponding seq assertions)

--- src/Faqt/StringAssertions.fs
+++ src/Faqt/StringAssertions.fs
@@ -35,63 +35,6 @@ module private Helpers =
 type StringAssertions =
 
 
-    // TODO: Remove this after the corresponding IEnumerable method has been implemented?
-    /// Asserts that the subject has the specified length.
-    [<Extension>]
-    static member HaveLength(t: Testable<string>, expected: int, ?because) : And<string> =
-        use _ = t.Assert()
-
-        if isNull t.Subject then
-            t.Fail("{subject}\n\tshould have length\n{0}\n\t{because}but was\n{actual}", because, format expected)
-        elif t.Subject.Length <> expected then
-            t.Fail(
-                "{subject}\n\tshould have length\n{0}\n\t{because}but length was\n{1}\n\n{actual}",
-                because,
-                format expected,
-                format t.Subject.Length
-            )
-
-        And(t)
-
-
-    // TODO: Remove this after the corresponding IEnumerable method has been implemented?
-    /// Asserts that the subject is empty. Equivalent to HaveLength(0) and Be("") (but with a different error message).
-    [<Extension>]
-    static member BeEmpty(t: Testable<string>, ?because) : And<string> =
-        use _ = t.Assert()
-
-        if isNull t.Subject || t.Subject.Length <> 0 then
-            t.Fail("{subject}\n\tshould be empty{because}, but was\n{actual}", because)
-
-        And(t)
-
-
-    // TODO: Remove this after the corresponding IEnumerable method has been implemented?
-    /// Asserts that the subject is not empty. Equivalent to NotBe("") (but with a different error message).
-    [<Extension>]
-    static member NotBeEmpty(t: Testable<string>, ?because) : And<string> =
-        use _ = t.Assert()
-
-        if isNull t.Subject then
-            t.Fail("{subject}\n\tshould not be empty{because}, but was\n{actual}", because)
-        elif t.Subject.Length = 0 then
-            t.Fail("{subject}\n\tshould not be empty{because}, but was empty.", because)
-
-        And(t)
-
-
-    // TODO: Remove this after the corresponding IEnumerable method has been implemented?
-    /// Asserts that the subject is null or empty.
-    [<Extension>]
-    static member BeNullOrEmpty(t: Testable<string>, ?because) : And<string> =
-        use _ = t.Assert()
-
-        if not (String.IsNullOrEmpty t.Subject) then
-            t.Fail("{subject}\n\tshould be null or empty{because}, but was\n{actual}", because)
-
-        And(t)
-
-
     /// Asserts that the subject is upper-case (i.e., that it is unchanged when calling ToUpper with the specified
     /// culture).
     [<Extension>]
