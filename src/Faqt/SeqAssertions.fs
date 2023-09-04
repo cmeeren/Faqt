a749469ef310b0e6c0e78ec13d22675b82a8766b
Christer van der Meeren <christer@infotech.no>
2023-09-04 14:17:44 +0200

    Fix SequenceEqual when only expected is null

--- src/Faqt/SeqAssertions.fs
+++ src/Faqt/SeqAssertions.fs
@@ -180,7 +180,7 @@ type SeqAssertions =
     static member SequenceEqual(t: Testable<#seq<'a>>, expected: seq<'a>, ?because) : And<_> =
         use _ = t.Assert()
 
-        if isNull (box t.Subject) && not (isNull expected) then
+        if isNull (box t.Subject) <> isNull expected then
             t.With("Expected", expected).With("But was", t.Subject).Fail(because)
         elif not (isNull (box t.Subject)) then
             let subjectLength = Seq.stringOptimizedLength t.Subject
