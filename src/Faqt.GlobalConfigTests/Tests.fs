5496bd3e05e96af53a46d77de6881244aaa0acf0
Christer van der Meeren <christer@infotech.no>
2023-09-06 11:14:28 +0200

    Add configuration tests

--- src/Faqt.GlobalConfigTests/Tests.fs
+++ src/Faqt.GlobalConfigTests/Tests.fs
@@ -1,12 +1,87 @@
 ﻿module Tests
 
+open System
+open System.Net.Http
+open System.Runtime.CompilerServices
 open Faqt
+open Faqt.AssertionHelpers
+open Faqt.Configuration
 open Faqt.Formatting
 open Xunit
 
 
+[<Extension>]
+type Assertions =
+
+
+    [<Extension>]
+    static member FailWith(t: Testable<'a>, key: string, value: 'b) : And<'a> =
+        use _ = t.Assert()
+        t.With(key, value).Fail(None)
+
+
 [<Fact>]
-let ``Can set, override and restore custom global formatter`` () =
+let ``Can set, override and restore custom global config and formatter`` () =
+    Config.Set(FaqtConfig.Default.SetHttpContentMaxLength(10))
+
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("lorem ipsum dolor sit amet")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsu…
+  [content truncated after 10 characters]
+"""
+
+    do
+        use _ = Config.With(FaqtConfig.Default.SetHttpContentMaxLength(20))
+
+        fun () ->
+            let x = new HttpRequestMessage(HttpMethod.Get, "/")
+            x.Version <- Version.Parse("0.5")
+            x.Content <- new StringContent("lorem ipsum dolor sit amet")
+            x.Should().FailWith("Value", x)
+        |> assertExnMsg
+            """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsum dolor si…
+  [content truncated after 20 characters]
+"""
+
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("lorem ipsum dolor sit amet")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsu…
+  [content truncated after 10 characters]
+"""
+
+
     Formatter.Set(fun _ -> "GLOBAL FORMATTER")
 
     fun () ->
---
+++ src/Faqt.Tests/Configuration.fs
@@ -0,0 +1,133 @@
+﻿module Configuration
+
+open System
+open System.Net.Http
+open Faqt
+open Faqt.Configuration
+open Xunit
+
+
+[<Fact>]
+let ``Can override and restore the default config`` () =
+
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("lorem ipsum dolor sit amet")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsum dolor sit amet
+"""
+
+    do
+        use _ = Config.With(FaqtConfig.Default.SetHttpContentMaxLength(10))
+
+        fun () ->
+            let x = new HttpRequestMessage(HttpMethod.Get, "/")
+            x.Version <- Version.Parse("0.5")
+            x.Content <- new StringContent("lorem ipsum dolor sit amet")
+            x.Should().FailWith("Value", x)
+        |> assertExnMsg
+            """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsu…
+  [content truncated after 10 characters]
+"""
+
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("lorem ipsum dolor sit amet")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsum dolor sit amet
+"""
+
+
+[<Fact>]
+let ``Can override and restore the default formatter in async code`` () =
+    async {
+
+        fun () ->
+            let x = new HttpRequestMessage(HttpMethod.Get, "/")
+            x.Version <- Version.Parse("0.5")
+            x.Content <- new StringContent("lorem ipsum dolor sit amet")
+            x.Should().FailWith("Value", x)
+        |> assertExnMsg
+            """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsum dolor sit amet
+"""
+
+        do
+            use _ = Config.With(FaqtConfig.Default.SetHttpContentMaxLength(10))
+
+            fun () ->
+                let x = new HttpRequestMessage(HttpMethod.Get, "/")
+                x.Version <- Version.Parse("0.5")
+                x.Content <- new StringContent("lorem ipsum dolor sit amet")
+                x.Should().FailWith("Value", x)
+            |> assertExnMsg
+                """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsu…
+  [content truncated after 10 characters]
+"""
+
+        fun () ->
+            let x = new HttpRequestMessage(HttpMethod.Get, "/")
+            x.Version <- Version.Parse("0.5")
+            x.Content <- new StringContent("lorem ipsum dolor sit amet")
+            x.Should().FailWith("Value", x)
+        |> assertExnMsg
+            """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: text/plain; charset=utf-8
+  Content-Length: 26
+
+  lorem ipsum dolor sit amet
+"""
+    }
+
+
+[<Fact>]
+let ``Default config`` () =
+    Assert.Equal(FaqtConfig.Default, Config.Current)
+    Assert.Equal(1024 * 1024, Config.Current.HttpContentMaxLength)
