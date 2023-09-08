9e86d6fb0bc468b788ca1db0fab5738a6778ffdb
Christer van der Meeren <christer@infotech.no>
2023-09-08 10:08:30 +0200

    Add optimized Contain for Set

---
+++ src/Faqt.Tests/SetAssertions.fs
@@ -0,0 +1,138 @@
+﻿module SetAssertions
+
+open Faqt
+open Xunit
+
+
+module Contain =
+
+
+    [<Fact>]
+    let ``Can be chained with AndDerived with found value`` () =
+        (set [ 1 ])
+            .Should()
+            .Contain(1)
+            .Id<AndDerived<Set<int>, int>>()
+            .That.Should()
+            .Be(1)
+
+
+    let passData = [
+        [| box (set [ "a" ]); "a" |]
+        [| set [ "a"; "b" ]; "a" |]
+        [| set [ (null: string) ]; (null: string) |]
+    ]
+
+
+    [<Theory>]
+    [<MemberData(nameof passData)>]
+    let ``Passes if set contains value`` (subject: Set<string>) (value: string) = subject.Should().Contain(value)
+
+
+    let failData = [
+        [| box Unchecked.defaultof<Set<string>>; "a" |]
+        [| Set.empty<string>; "a" |]
+        [| set [ "a" ]; "b" |]
+        [| set [ (null: string) ]; "a" |]
+        [| set [ "a" ]; (null: string) |]
+    ]
+
+
+    [<Theory>]
+    [<MemberData(nameof failData)>]
+    let ``Fails if null or not containing value`` (subject: Set<string>) (value: string) =
+        assertFails (fun () -> subject.Should().Contain(value))
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = Set.empty<int>
+            x.Should().Contain(1)
+        |> assertExnMsg
+            """
+Subject: x
+Should: Contain
+Item: 1
+But was: []
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = Set.empty<int>
+            x.Should().Contain(1, "Some reason")
+        |> assertExnMsg
+            """
+Subject: x
+Because: Some reason
+Should: Contain
+Item: 1
+But was: []
+"""
+
+
+module NotContain =
+
+
+    [<Fact>]
+    let ``Can be chained with And`` () =
+        (set [ 1 ]).Should().NotContain(2).Id<And<Set<int>>>().And.Be(set [ 1 ])
+
+
+    let passData = [
+        [| box Unchecked.defaultof<Set<string>>; "a" |]
+        [| Set.empty<string>; "a" |]
+        [| set [ "a" ]; "b" |]
+        [| set [ (null: string) ]; "a" |]
+        [| set [ "a" ]; (null: string) |]
+    ]
+
+
+    [<Theory>]
+    [<MemberData(nameof passData)>]
+    let ``Passes if null or not containing value`` (subject: Set<string>) (value: string) =
+        subject.Should().NotContain(value)
+
+
+    let failData = [
+        [| box (set [ "a" ]); "a" |]
+        [| set [ "a"; "b" ]; "a" |]
+        [| set [ (null: string) ]; (null: string) |]
+    ]
+
+
+    [<Theory>]
+    [<MemberData(nameof failData)>]
+    let ``Fails if sequence contains value`` (subject: Set<string>) (value: string) =
+        assertFails (fun () -> subject.Should().NotContain(value))
+
+
+    [<Fact>]
+    let ``Fails with expected message`` () =
+        fun () ->
+            let x = set [ 1; 2 ]
+            x.Should().NotContain(2)
+        |> assertExnMsg
+            """
+Subject: x
+Should: NotContain
+Item: 2
+But was: [1, 2]
+"""
+
+
+    [<Fact>]
+    let ``Fails with expected message with because`` () =
+        fun () ->
+            let x = set [ 1; 2 ]
+            x.Should().NotContain(2, "Some reason")
+        |> assertExnMsg
+            """
+Subject: x
+Because: Some reason
+Should: NotContain
+Item: 2
+But was: [1, 2]
+"""
---
+++ src/Faqt/SetAssertions.fs
@@ -0,0 +1,30 @@
+﻿namespace Faqt
+
+open System.Runtime.CompilerServices
+open Faqt.AssertionHelpers
+
+
+[<Extension>]
+type SetAssertions =
+
+
+    /// Asserts that the subject contains the specified item.
+    [<Extension>]
+    static member Contain(t: Testable<Set<'a>>, item: 'a, ?because) : AndDerived<_, 'a> =
+        use _ = t.Assert()
+
+        if isNull (box t.Subject) || not (Set.contains item t.Subject) then
+            t.With("Item", item).With("But was", t.Subject).Fail(because)
+
+        AndDerived(t, item)
+
+
+    /// Asserts that the subject does not contain the specified item. Passes if the subject is null.
+    [<Extension>]
+    static member NotContain(t: Testable<Set<'a>>, item: 'a, ?because) : And<_> =
+        use _ = t.Assert()
+
+        if not (isNull (box t.Subject)) && Set.contains item t.Subject then
+            t.With("Item", item).With("But was", t.Subject).Fail(because)
+
+        And(t)
