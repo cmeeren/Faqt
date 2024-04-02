1d47aad0f14d620e061e9a5fa602f74e1bd2df7a
Christer van der Meeren <christer@infotech.no>
2024-04-02 15:58:04 +0200

    Add HTTP content formatting tests

--- src/Faqt.Tests/Configuration.fs
+++ src/Faqt.Tests/Configuration.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Net.Http
+open System.Net.Http.Headers
 open Faqt
 open Faqt.Configuration
 open Xunit
@@ -128,6 +129,51 @@ Value: |-
   lorem ipsum dolor sit amet
 """
     }
+
+
+[<Fact>]
+let ``HTTP content formatting is on by default`` () =
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("{\"a\":1}")
+        x.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: application/json
+  Content-Length: 7
+
+  [content has been formatted]
+  {"a": 1}
+"""
+
+
+[<Fact>]
+let ``HTTP content formatting can be turned off`` () =
+    use _ = Config.With(FaqtConfig.Default.SetFormatHttpContent(false))
+
+    fun () ->
+        let x = new HttpRequestMessage(HttpMethod.Get, "/")
+        x.Version <- Version.Parse("0.5")
+        x.Content <- new StringContent("{\"a\":1}")
+        x.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
+        x.Should().FailWith("Value", x)
+    |> assertExnMsg
+        """
+Subject: x
+Should: FailWith
+Value: |-
+  GET / HTTP/0.5
+  Content-Type: application/json
+  Content-Length: 7
+
+  {"a":1}
+"""
 
 
 [<Fact>]
